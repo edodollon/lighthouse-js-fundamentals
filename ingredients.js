ecb0e3d8a44df07f3444a198aab6e323ac963443
Jomel Edodollon <jomeledodollon@yahoo.ca>
2021-12-28 23:12:17 -0800

    answers to challenges

--- ingredients.js
+++ ingredients.js
@@ -1,3 +1,4 @@
+/*
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
 // Write a while loop that prints out the contents of ingredients:
@@ -14,3 +15,25 @@ for (let x = 0; x < ingredients.length; x++) {
 for (let x = ingredients.length -1 ; x >= 0; x--) {
   console.log(ingredients[x]);
 }
+*/
+
+
+function lastIndexOf (array, num) {
+  return array.lastIndexOf(num);
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
+
+function concat (array1, array2) {
+  let array3 = array1.concat(array2);
+  return array3;
+};
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
